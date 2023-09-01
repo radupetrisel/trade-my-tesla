f3e0487370f5bceadf72a0643aa6309dbdac692e
Radu Petrisel <radupetrisel@gmail.com>
2023-09-01 09:55:38 +0300

    Initial Commit

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# Trade my Tesla
+
+This is a tutorial app from Advanced Swift: Volume II by Paul Hudson. This tutorial focuses on using CreateML generated ML models in iOS apps.
---
+++ Trade My Tesla/Trade_My_TeslaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trade_My_TeslaApp.swift
+//  Trade My Tesla
+//
+//  Created by Radu Petrisel on 01.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct Trade_My_TeslaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
